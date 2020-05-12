ee89ebde7a3f9f429cfc4eecc21493bf771213a6
cedrictaro-g <cedric.tarou@gmail.com>
2020-05-12 20:58:43 +0900

    fixed errors and refactoring

--- src/components/GameDisplay.vue
+++ src/components/GameDisplay.vue
@@ -43,8 +43,11 @@ export default {
         if (this.isActive) {
           return;
         }
+        //checkAnwerの初期化したいけどうまく動かない
+        // window.removeEventListener("keydown", e => {
+        //   this.checkAnswer(e);
+        // });
         //isActiveがfalseときはゲームをスタートする
-
         this.startGame();
       }
     });
@@ -54,9 +57,9 @@ export default {
       this.loc = 0;
       this.score = 0;
       this.miss = 0;
-      this.isActive = false;
     },
     startGame() {
+      this.init();
       this.isActive = true;
       //クリックしたときの時間を保持する
       this.startTime = Date.now();
@@ -73,20 +76,13 @@ export default {
 
       //ゲームオーバーの設定
       if (timeLeft < 0) {
-        this.isActive = false;
         clearTimeout(timeoutId);
         this.timer = "0.00";
-
-        //showResultが実行されてからinitを実行する
-        const promise = new Promise(resolve => {
-          setTimeout(() => {
-            this.showResult();
-            resolve();
-          }, 100);
-        });
-        promise.then(() => {
-          this.init();
-        });
+        // timeLeftの値を0.00がになってからshowResultになってほしいので単純にタイミングをずらしました。
+        setTimeout(() => {
+          this.showResult();
+          this.retry();
+        }, 100);
       }
     },
     showResult() {
@@ -94,10 +90,20 @@ export default {
         this.score + this.miss === 0
           ? 0
           : (this.score / (this.score + this.miss)) * 100;
-      const msg = `Score: ${this.score}, Miss: ${
+      const result = `Score: ${this.score}, Miss: ${
         this.miss
       }, Accuary :${accuracy.toFixed(2)}%`;
-      alert(msg);
+      alert(result);
+    },
+    retry() {
+      const msg = "Try again?";
+      const reply = confirm(msg);
+      if (reply) {
+        this.init();
+        this.isActive = false;
+        //強制的にリロードさせる
+        location.reload();
+      }
     },
     makeQuiz() {
       // ランダムに単語が選ばれるようにする;
@@ -106,7 +112,7 @@ export default {
       return this.quiz;
     },
     updateTarget() {
-      //正解したもじを＿に変えていく処理
+      //正解した文字を＿に変えていく処理
       let placeholder = "";
       for (let i = 0; i < this.loc; i++) {
         placeholder += "_";
@@ -114,24 +120,29 @@ export default {
       this.quiz = placeholder + this.quiz.substring(this.loc);
     },
     typeWord() {
+      //ゲームが始まっていなかったら処理しない
+      if (!this.isActive) {
+        return;
+      }
+      //ゲームが始まっていたら、ボタンを認識する
       window.addEventListener("keydown", e => {
-        if (!this.isActive) {
-          return;
-        }
-        // タイピング判定の処理
-        if (e.key === this.quiz[this.loc]) {
-          this.loc++;
-          //locがquizと同じ数になるまで進むと、次の単語になるように処理する。
-          if (this.loc === this.quiz.length) {
-            this.makeQuiz();
-            this.loc = 0;
-          }
-          this.updateTarget();
-          this.score++;
-        } else {
-          this.miss++;
-        }
+        this.checkAnswer(e);
       });
+    },
+    checkAnswer(e) {
+      // タイピング判定の処理
+      if (e.key === this.quiz[this.loc]) {
+        this.loc++;
+        //locがquizと同じ数になるまで進むと、次の単語になるように処理する。
+        if (this.loc === this.quiz.length) {
+          this.makeQuiz();
+          this.loc = 0;
+        }
+        this.updateTarget();
+        this.score++;
+      } else {
+        this.miss++;
+      }
     }
   }
 };
