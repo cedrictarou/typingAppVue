770fbd9efdc9a128ce98851b4e8a9659e8d4ee69
cedrictaro-g <cedric.tarou@gmail.com>
2020-05-13 16:29:26 +0900

    first commit for login page

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <div>
+      <form action="">
+        <div>
+          <label for="">アカウント名 </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label for="">メールアドレス</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label for="">パスワード</label>
+          <input type="password" />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style></style>
